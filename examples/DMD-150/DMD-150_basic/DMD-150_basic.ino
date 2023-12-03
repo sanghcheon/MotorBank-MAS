2b4e5925699f3f39fc59369758b40cd3ee3c65b2
Sangheui-Cheon <sang.h.cheon@snu.ac.kr>
2023-12-03 21:54:32 +0900

    dmd-150 update

--- examples/DMD-150/DMD-150_basic/DMD-150_basic.ino
+++ examples/DMD-150/DMD-150_basic/DMD-150_basic.ino
@@ -5,20 +5,22 @@ MAS001 myShield;
 DMD150 myMotor;
 
 void setup() {
+  Serial.begin(115200);
 
 }
 
 int val;
 void loop() {
-  val = myShield.getPot();
+  val = myShield.getPot() / 4; // rotation funtion takes input from -255 ~ 255
+  Serial.println(val);
   
   if(myShield.button1Clicked()){
   	myMotor.floating(); // Use floating function to soft stop
   	// myMotor.brake(); // Use brake function to hard stop
   }else if(myShield.button2Clicked()){
-  	// rotation funtion takes input from -255 ~ 255
-  	myMotor.rotation(-val / 4);
+  	myMotor.rotation(-val);
   }else{
-  	myMotor.rotation(val / 4);
+  	myMotor.rotation(val)
+    ;
   }
 }
